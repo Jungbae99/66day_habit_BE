24571e9d2ae7c3ff51bdfdd516d8456480e62791
jungbaejeon <wjdqotktls55@gmail.com>
2023-10-07 17:06:41 +0900

    Feat/ custom validation
    - 이메일, 비밀번호 정규식으로 백엔드서버에서 2차적으로 validation 적용했습니다.
    
    - enum type validation 적용하는 어노테이션 구현

---
+++ src/main/java/day/dayBackend/util/validation/enums/EnumValid.java
@@ -0,0 +1,19 @@
+package day.dayBackend.util.validation.enums;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = EnumValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EnumValid {
+    String message() default "{validation.EnumValid}";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+    Class<? extends java.lang.Enum<?>> enumClass();
+}
