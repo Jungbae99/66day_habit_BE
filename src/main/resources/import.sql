8e72a1e9212dbc38ec3c9904c9bd8b5660030c8b
jungbaejeon <wjdqotktls55@gmail.com>
2023-10-11 22:26:13 +0900

    docs/ 문서 수정
    - 시크릿 파일들 넣을 공간 .gitignore 추가했습니다.
    - 스프링 시큐리티 & jwt 의존성 추가했습니다.
    - 애플리케이션 시작하면서 권한 추가했습니다. (import.sql)

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+-- 권한
+insert into authority (`id`, `created_at`, `authority_name`) values ('1', TIMESTAMP '2023-10-11 00:00:00.123456', 'ROLE_ADMIN');
+insert into authority (`id`, `created_at`, `authority_name`) values ('2', TIMESTAMP '2023-10-11 00:00:00.123456', 'ROLE_MANAGER');
+insert into authority (`id`, `created_at`, `authority_name`) values ('3', TIMESTAMP '2023-10-11 00:00:00.123456', 'ROLE_USER');
