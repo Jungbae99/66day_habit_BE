eb755f4fc514723fe2115b72bf99bcf40b44d398
jungbaejeon <wjdqotktls55@gmail.com>
2023-10-13 17:19:26 +0900

    Feat/ 토큰 리프레쉬
    - 쿠키를 받아서 엑세스토큰과 리프레쉬토큰을 변경합니다.

--- src/main/java/day/dayBackend/controller/AuthController.java
+++ src/main/java/day/dayBackend/controller/AuthController.java
@@ -3,12 +3,13 @@ package day.dayBackend.controller;
 import day.dayBackend.dto.request.DirectSignInRequestDto;
 import day.dayBackend.dto.response.CommonResponseDto;
 import day.dayBackend.dto.response.SignInResponseDto;
+import day.dayBackend.jwt.TokenDto;
+import day.dayBackend.jwt.TokenProvider;
 import day.dayBackend.service.AuthService;
+import jakarta.servlet.http.Cookie;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,16 +21,35 @@ public class AuthController {
 
     private final AuthService authService;
 
+    /**
+     * 이메일 로그인
+     */
     @PostMapping("/signIn/direct")
     public ResponseEntity<CommonResponseDto<SignInResponseDto>> directSignInV1(@Valid @RequestBody final DirectSignInRequestDto dto) {
         SignInResponseDto signInResponseDto = authService.signIn(dto);
-        String refToken = signInResponseDto.getResponseCookie().toString();
+        String refCookie = signInResponseDto.getResponseCookie().toString();
         signInResponseDto.setResponseCookie(null);
 
         return ResponseEntity.ok()
-                .header(HttpHeaders.SET_COOKIE, refToken)
+                .header(HttpHeaders.SET_COOKIE, refCookie)
                 .body(CommonResponseDto.<SignInResponseDto>builder()
                         .data(signInResponseDto)
+                        .build());
+    }
+
+    /**
+     * 토큰 리프레시
+     */
+    @PostMapping("/refresh")
+    public ResponseEntity<CommonResponseDto<TokenDto>> refreshTokenV1(@CookieValue Cookie jwtCookie) {
+        TokenDto tokenDto = authService.tokenRefresh(jwtCookie.getValue());
+        String refCookie = tokenDto.getRefreshTokenCookie().toString();
+        tokenDto.setRefreshTokenCookie(null);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, refCookie)
+                .body(CommonResponseDto.<TokenDto>builder()
+                        .data(tokenDto)
                         .build());
     }
 }
