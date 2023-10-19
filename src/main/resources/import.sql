ab2e896ff1bd69b2725cdafbc238e8744f1c960b
jungbaejeon <wjdqotktls55@gmail.com>
2023-10-19 17:01:29 +0900

    Fix/ 기본 이미지 추가
    - 회원가입시 기본 이미지가 생성됩니다.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,8 +6,15 @@ insert into authority (`created_at`, `authority_name`) values (TIMESTAMP '2023-1
 -- 운영자
 INSERT INTO member (`created_at`, `certified`, `email`, `introduction`, `password`, `username`) VALUES (TIMESTAMP '2023-10-11 00:00:00.123456', 'CERTIFIED', 'admin@admin.com', '운영자에요', '$2a$10$xvKKHfryO0OPVJgAq9xhQeZWqAvPkN7Z5CDXs3utdsFVathqxuoFG', '운영자');
 
+-- 업로드(기본 프로필 이미지)
+insert into upload (`created_at`, `extension`, `file_category`, `origin_name`, `saved_name`, `size`, `type`, `url`, `member_id`) values (TIMESTAMP '2023-10-11 00:00:00.123456', 'png', 'PROFILE', 'user.png', '3d614926-3d3c-472d-abe0-42baff030486-1697700625400.png', 16034, 'image/png' ,'https://66day-bucket.s3.ap-northeast-2.amazonaws.com/profile/3d614926-3d3c-472d-abe0-42baff030486-1697700625400.png', 1);
+
+update member set `profile_image_id` = 1 where `member_id` = 1;
+
 -- 유저
-INSERT INTO member (`created_at`, `certified`, `email`, `introduction`, `password`, `username`) VALUES (TIMESTAMP '2023-10-11 00:00:00.123456', 'CERTIFIED', 'user@user.com', '유저에요', '$2a$10$C9qR.UDIavY5A4u7defjae5pU4R2HoLDwE1XGxOZDXy/lj2YGQPWG', '유저');
+INSERT INTO member (`created_at`, `certified`, `email`, `introduction`, `password`, `username`, `profile_image_id`) VALUES (TIMESTAMP '2023-10-11 00:00:00.123456', 'CERTIFIED', 'user@user.com', '유저에요', '$2a$10$C9qR.UDIavY5A4u7defjae5pU4R2HoLDwE1XGxOZDXy/lj2YGQPWG', '유저', 1);
+
+
 
 -- 권한
 insert into member_authority (`created_at`, `member_id`, `authority_id`) values (TIMESTAMP '2023-10-11 00:00:00.123456', 1, 1);
