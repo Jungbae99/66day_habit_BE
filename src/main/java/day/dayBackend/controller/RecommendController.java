e0b33c5561eaccfb3a3e742023118541165b6f48
jungbaejeon <wjdqotktls55@gmail.com>
2023-11-09 13:28:48 +0900

    HotFix/ 크롤링 Cors

--- src/main/java/day/dayBackend/controller/RecommendController.java
+++ src/main/java/day/dayBackend/controller/RecommendController.java
@@ -11,14 +11,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@CrossOrigin(origins = "https://localhost:3000", allowedHeaders = "*", allowCredentials = "true")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/recommend")
