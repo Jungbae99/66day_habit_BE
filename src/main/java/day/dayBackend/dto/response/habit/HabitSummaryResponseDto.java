878e780311610213b13fd952db68b3b1fe09827e
jungbaejeon <wjdqotktls55@gmail.com>
2023-11-16 13:21:49 +0900

    Fix/ 습관 Response 스펙 변경
    - 습관응답시 이름, 이미지를 추가했습니다.

--- src/main/java/day/dayBackend/dto/response/habit/HabitSummaryResponseDto.java
+++ src/main/java/day/dayBackend/dto/response/habit/HabitSummaryResponseDto.java
@@ -9,6 +9,9 @@ import java.util.List;
 public class HabitSummaryResponseDto {
 
     private Long memberId;
+    private String username;
+    private String profileImage;
+    private String backgroundImage;
     private Long habitId;
     private String habitName;
     private String backgroundColor;
@@ -20,6 +23,9 @@ public class HabitSummaryResponseDto {
     public static HabitSummaryResponseDto fromEntity(Habit entity) {
         HabitSummaryResponseDto dto = new HabitSummaryResponseDto();
         dto.memberId = entity.getMember().getId();
+        dto.username = entity.getMember().getUsername();
+        dto.profileImage = entity.getMember().getProfileImage().getUrl();
+        dto.backgroundImage = entity.getMember().getBackgroundImage().getUrl();
         dto.habitId = entity.getId();
         dto.habitName = entity.getHabitName();
         dto.backgroundColor = entity.getBackgroundColor().toString();
