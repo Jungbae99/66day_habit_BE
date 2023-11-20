741b44d0395ab993906984523b2771d4c95cbab1
jungbaejeon <wjdqotktls55@gmail.com>
2023-11-20 16:17:12 +0900

    Feat/ 채팅 관련 엔티티

---
+++ src/main/java/day/dayBackend/domain/chat/ChatRoom.java
@@ -0,0 +1,56 @@
+package day.dayBackend.domain.chat;
+
+import day.dayBackend.domain.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Member sender;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Member receiver;
+
+    private LocalDateTime senderDeletedAt;
+
+    private LocalDateTime receiverDeletedAt;
+
+    private LocalDateTime senderChatDeletedAt;
+
+    private LocalDateTime receiverChatDeletedAt;
+
+    @Builder
+    ChatRoom(Long id, Member sender, Member receiver) {
+        this.id = id;
+        this.sender = sender;
+        this.receiver = receiver;
+    }
+
+    public void deleteSender() {
+        this.senderDeletedAt = LocalDateTime.now();
+        this.senderChatDeletedAt = LocalDateTime.now();
+    }
+
+    public void deleteReceiver() {
+        this.receiverDeletedAt = LocalDateTime.now();
+        this.receiverChatDeletedAt = LocalDateTime.now();
+    }
+
+    public void updateDeletedAt() {
+        this.senderDeletedAt = null;
+        this.receiverDeletedAt = null;
+    }
+}
