25e66559c766c6bd783fc2999ae49bd5cc35f8aa
jungbaejeon <wjdqotktls55@gmail.com>
2023-12-03 11:03:55 +0900

    Fix/ 크롤링 RDS 연결

--- src/main/resources/python/QuoteCrawling.py
+++ src/main/resources/python/QuoteCrawling.py
@@ -1,14 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 import pymysql
+import yaml
 
-db_connection = pymysql.connect (
-    host = 'localhost',
-    user = '66day',
-    password = 'habit1102!',
-    database = 'habit66',
-    charset='utf8mb4'
-)
+# application.yml 파일의 경로 지정
+yml_path = 'src/main/resources/application.yml'
+
+# application.yml 파일 읽어오기
+with open(yml_path, 'r') as ymlfile:
+    cfg = yaml.safe_load(ymlfile)
+
+# 데이터베이스 연결 설정
+db_config = {
+    'host': cfg['spring']['datasource']['url'].split('//')[1].split(':')[0],
+    'user': cfg['spring']['datasource']['username'],
+    'password': cfg['spring']['datasource']['password'],
+    'database': cfg['spring']['datasource']['url'].split('/')[-1].split('?')[0],
+    'charset': 'utf8mb4'
+}
+
 
 # 크롤링할 웹사이트 URL
 url = 'https://insight-up.tistory.com/91'
@@ -39,16 +49,19 @@ if response.status_code == 200:
                     'wise_saying': wise_saying,
                     'great_person': great_person
                 })
-    cursor = db_connection.cursor()
 
-    for data in quotes:
-        wise_saying = data['wise_saying']
-        great_person = data['great_person']
+    db_connection = pymysql.connect(**db_config)
+    try:
+        cursor = db_connection.cursor()
 
-        insert_query = "INSERT INTO Recommended_Quote (wise_saying, great_person) VALUES (%s, %s)"
-        cursor.execute(insert_query, (wise_saying, great_person))
+        for data in quotes:
+            wise_saying = data['wise_saying']
+            great_person = data['great_person']
 
-    db_connection.commit()
+            insert_query = "INSERT INTO Recommended_Quote (wise_saying, great_person) VALUES (%s, %s)"
+            cursor.execute(insert_query, (wise_saying, great_person))
 
-    cursor.close()
-    db_connection.close()
+        db_connection.commit()
+    finally:
+        cursor.close()
+        db_connection.close()
