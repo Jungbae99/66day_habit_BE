9eec374c7531ea6efc1ff7ee6bdca2a89ab0e129
jungbaejeon <wjdqotktls55@gmail.com>
2023-12-06 00:16:48 +0900

    Feat/ CORS 설정

---
+++ src/main/java/day/dayBackend/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package day.dayBackend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    private final String serverUrl;
+    private final String clientUrl;
+
+    public CorsConfig(
+            @Value("${deploy.client.url}") String serverUrl,
+            @Value("${deploy.server.url}") String clientUrl) {
+        this.serverUrl = serverUrl;
+        this.clientUrl = clientUrl;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(serverUrl, clientUrl)
+                .allowedMethods("*")
+                .allowCredentials(true)
+                .maxAge(3000)
+                .allowedHeaders("*");
+    }
+
+}
